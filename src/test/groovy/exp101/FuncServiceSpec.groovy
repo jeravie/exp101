59c0e88ec200f5ea70bee21029bc40ce4a745864
jeravie <52323445+jeravie@users.noreply.github.com>
2020-03-03 15:00:35 +0200

    Update FuncServiceSpec.groovy

--- src/test/groovy/exp101/FuncServiceSpec.groovy
+++ src/test/groovy/exp101/FuncServiceSpec.groovy
@@ -22,6 +22,35 @@ class FuncServiceSpec extends Specification implements ServiceUnitTest<FuncServi
             service.getRunningBalance(expUser) == 500
 
     }
+
+    void "test getRunningBalanceUpUntilBeforeTransaction"() {
+        def expUser = new ExpUser(userName: 'Gerry')
+        def tx = new Transaction(amountZAR: 250)
+        def tx2 = new Transaction(amountZAR: 500)
+        expUser.addToTransactions(tx)
+        expUser.addToTransactions(tx2)
+        expUser.save()
+
+        expUserTransactions = expUser.transactions
+
+        expect: 
+            service.getRunningBalanceUpUntilBeforeTransaction(tx2, expUserTransactions) == 250
+    }
+
+    void "test recalculateFollowingTransactionRunningBalances"() {
+        def expUser = new ExpUser(userName: 'Gerry')
+        def tx = new Transaction(amountZAR: 250)
+        def tx2 = new Transaction(amountZAR: 500)
+        expUser.addToTransactions(tx)
+        expUser.addToTransactions(tx2)
+        expUser.save()
+
+        expUserTransactions = expUser.transactions
+
+        service.recalculateFollowingTransactionRunningBalances(tx, expUserTransactions)
+        expect: 
+            tx2.runningBalance == 500
+    }
     */
     
     
